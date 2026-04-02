bb94a06a43f3f40716395f76133675abbff646f5
ScriptLine <scriptlinestudios@protonmail.com>
2026-04-02 16:03:14 +0200

    boincification done??

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@ nvcc -g -maxrregcount 128 --resource-usage -lineinfo -Xptxas -lineinfo -v -O3 -a
         main.cu -m64 -o cuda \
         -DBOINC -Iboinc/ -Lboinc/lib/lin -lcuda -lboinc_api -lboinc -Xptxas -v
         ls -la
+
+g++ -o cl opencl_main.cpp -lOpenCL \
+        -O3 \
+        -march=native \
+        -DCL_HPP_TARGET_OPENCL_VERSION=300 \
+        -DBOINC -Iboinc/ -Lboinc/lib/lin -lcuda -lboinc_api -lboinc
