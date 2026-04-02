dfdf87112611cc69fdcd8b81d9a3661a24c94cfc
ScriptLine <scriptlinestudios@protonmail.com>
2026-04-02 21:30:48 +0200

    minimum version

--- opencl_main.cpp
+++ opencl_main.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 
 #define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+
 #if __has_include(<CL/opencl.hpp>)
   #include <CL/opencl.hpp>
 #elif __has_include(<CL/cl2.hpp>)
