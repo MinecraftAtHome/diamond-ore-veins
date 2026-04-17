c7809755f4d17b6c8fabc9b116641096d26571c3
ScriptLine <scriptlinestudios@protonmail.com>
2026-04-17 03:08:20 +0200

    revert back to 2^26

--- opencl_main.cpp
+++ opencl_main.cpp
@@ -405,7 +405,7 @@ int main(int argc, char **argv) {
     seed_kernel.setArg(2, result_count);
     // opencl setup end
 
-    cl::NDRange global_size(1ull << 30);
+    cl::NDRange global_size(1ull << 26);
     cl::NDRange local_size(256);
     
     auto start = high_resolution_clock::now();
@@ -413,8 +413,8 @@ int main(int argc, char **argv) {
     FILE* seedsout = fopen("seeds.txt", "a");
     for (uint64_t s = (uint64_t)block_min + offsetStart; s < (uint64_t)block_max; s++) {
         // kernel launch start
-        for (uint64_t i = 0; i < 4; i++) {
-            uint64_t o = (s * (1ull << 32)) + (i * (1ull << 30));
+        for (uint64_t i = 0; i < 64; i++) {
+            uint64_t o = (s * (1ull << 32)) + (i * (1ull << 26));
             seed_kernel.setArg(0, o);
             err = queue.enqueueNDRangeKernel(seed_kernel, cl::NullRange, global_size, local_size);
             if (err != CL_SUCCESS) {
